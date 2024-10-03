4ec53077658b79f8457265c1d2ed25561f5887d7
Minhaz Hussain <truluxyofficial@gmail.com>
2024-10-03 08:28:54 +0600

    lolo

--- restaurant/restaurant.js
+++ restaurant/restaurant.js
@@ -1,6 +1,11 @@
 const hamburgerButton = document.querySelector('.hamburger-logo')
 const navbar = document.querySelector('.navbar')
+const cross = document.querySelector('.cross')
 
 hamburgerButton.addEventListener('click', () => {
-    navbar.classList.toggle('hamburger') 
+    navbar.parentElement.classList.toggle('show')
+})
+
+cross.addEventListener('click', () => {
+    navbar.parentElement.classList.remove('show')
 })
